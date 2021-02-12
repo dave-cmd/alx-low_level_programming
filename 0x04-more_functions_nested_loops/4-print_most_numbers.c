257d09da0f0bd317a26e518228ea50b80f7e0313
dave-cmd <kanjurus8@gmail.com>
2021-02-12 11:05:58 +0000

    Q4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - prints 0-9, except 2 and 4
+ *
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+  int number = 0;
+  while (number < 10)
+{
+  if (number != 2 && nuber != 4)
+{
+  _putchar(number)}
+}
+_putchar("\n");
+}
