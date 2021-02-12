68c469c9099efe9c845c44e2174b00fe9d201b89
dave-cmd <kanjurus8@gmail.com>
2021-02-12 12:21:27 +0000

    Q7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * print_line - draws a straight line in the terminal based on input
+ *
+ * @n: length of line
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+  while (n != 0){
+    if (n == 1){
+      _putchar(92);
+    }
+    else {
+      int x;
+      x = n-1;
+      while(x > 0){
+	_putchar(' ');
+	x = x - 1;
+      }
+      _putchar(92);
+
+    }
+    n = n - 1;
+ }
+}
