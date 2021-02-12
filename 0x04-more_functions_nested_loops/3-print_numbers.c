858db08344ba2ffaa58eb1c6de5b5b711a92a93a
dave-cmd <kanjurus8@gmail.com>
2021-02-12 10:34:14 +0000

    Q3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * print_numbers - Prints 0-9 using _putchar function
+ *
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+_putchar(i);
+}
+_putchar("\n");
+}
