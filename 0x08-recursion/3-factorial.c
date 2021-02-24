c951426fce855c132a036f4d8f83ca8e94d0801f
dave-cmd <kanjurus8@gmail.com>
2021-02-24 19:14:58 +0000

    Bulk commit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * factorial - get factorial of a number, recursively
+ *
+ *
+ * @n: number to get factorial of
+ * Return: factorial of n, as an int
+ */
+int factorial(int n)
+{
+int fact;
+fact = 0;
+if (n < 0)
+return (-1);
+if (n == 0)
+return (1);
+if (n == 1)
+return (1);
+fact = n * factorial(n - 1);
+return (fact);
+}
