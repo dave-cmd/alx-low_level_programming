4efabb293519cf41a73f30b6c5fec6f76d70283a
dave-cmd <kanjurus8@gmail.com>
2021-03-03 06:16:41 +0000

    Q0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,18 +1,50 @@
 #include "holberton.h"
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "holberton.h"
 
 /**
- * malloc_checked - mallocs memory asked, exits with code 98 on failure
+ * _memset - fills memory with constant bytes
  *
- * @b: memory required
- * Return: pointer to memory requested
+ * @s: location to fill
+ * @b: char to fill location with
+ * @n: number of bytes to fill
+ * Return: returns pointer to location filled
  */
-
-void *malloc_checked(unsigned int b)
+char *_memset(char *s, char b, unsigned int n)
 {
-void *pointer;
-pointer = malloc(b);
-if (pointer == NULL)
-exit(98);
-return (pointer);
+int i;
+i = 0;
+while (n > 0)
+{
+s[i] = b;
+i++;
+n--;
+}
+return (s);
+}
+
+/**
+ * create_array - creates an array of chars,  and initalizes it with specific
+ * characters
+ *
+ * @size: size of array
+ * @c: character to initialize array with
+ * Return: returns pointer to array
+ */
+char *create_array(unsigned int size, char c)
+{
+char *str;
+int arsize;
+
+if (size <= 0)
+return (NULL);
+arsize = (sizeof(char) * (int)size);
+str = malloc(arsize);
+if (str == NULL)
+return (NULL);
+_memset(str, c, size);
+return (str);
 }
