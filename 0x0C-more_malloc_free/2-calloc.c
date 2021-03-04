0f729372fcd1df7ce336d121473cd6570eaccaa8
dave-cmd <kanjurus8@gmail.com>
2021-03-04 20:37:50 +0000

    changes

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
   if (nmemb == 0 || size == 0)
     return (NULL);
-  space = malloc(nmemb * size);
+  space = malloc(nmemb * size * 4);
   if (space == NULL)
     return (NULL);
   for (i = 0; i != size; i++)
